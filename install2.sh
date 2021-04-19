89a075013c85149fa53509111d99446f69bb0bc0
ViralWeaponry <68037525+ViralWeaponry@users.noreply.github.com>
2021-04-18 20:57:04 -0400

    Create install2.sh

---
+++ install2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+./startubuntu.sh
+
+apt update
+
+apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
+
+git clone git://github.com/xmrig/xmrig.git
+
+cd xmrig
+
+mkdir build
+
+cd build
+
+apt-get update
+
+apt-get install libhwloc-dev
+
+cmake ..
+
+make
