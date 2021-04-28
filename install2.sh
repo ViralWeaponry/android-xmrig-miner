a04202d668c86ea48d3e0997f61922b862861f15
ViralWeaponry <68037525+ViralWeaponry@users.noreply.github.com>
2021-04-28 10:28:40 -0400

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -4,7 +4,7 @@
 
 apt update
 
-apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev
+apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-dev -y
 
 git clone git://github.com/xmrig/xmrig.git
 
@@ -16,7 +16,7 @@ cd build
 
 apt-get update
 
-apt-get install libhwloc-dev
+apt-get install libhwloc-dev -y
 
 cmake ..
 
